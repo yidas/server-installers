71b5dd2a23b79cf469f26fb1d67b361769212f21
Nick Tsai <myintaer@gmail.com>
2019-04-15 13:46:13 +0800

    D18

---
+++ python-web-server/django-nginx-uwsgi_ubuntu18.04.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Python Nginx+uWSGI Installer for Ubuntu 16.04 TLS
+#
+# @author  Nick Tsai <myintaer@gmail.com>
+# @version 1.0.0
+# @link    https://github.com/yidas/server-installers
+
+# APT Source using IPv4
+sudo apt-get update
+
+# Nginx
+sudo apt-get install nginx -y
+
+# Python 3 with uWSGI
+sudo apt-get install python3 python3-pip -y
+python3 -m pip install --upgrade pip
+python3 -m pip install uwsgi
+
+# Django
+python3 -m pip install Django
+django-admin startproject mysite
+
+projectDir="/var/www/"
+projectName="mysite"
+projectPath="${projectDir}${projectName}/"
+# Create project
+cd "${projectDir}"
+django-admin startproject "${projectName}"
+# Config Django
+sed -i '/DEBUG = True/c\DEBUG = False' "${projectPath}mysite/settings.py"
+sed -i "/ALLOWED_HOSTS = \[\]/c\ALLOWED_HOSTS = \['*'\]" "${projectPath}mysite/settings.py"
+
+# Nginx Default Site
+configUrl='https://raw.githubusercontent.com/yidas/server-installers/master/python-web-server/nginx-sites/deafult-uwsgi'
+sudo wget "${configUrl}" -O /etc/nginx/sites-available/default
+
+# Run uWSGI in the backgroud (You can set a ini config for WSGI)
+uwsgi --socket :8001 --chdir "${projectPath}" --wsgi-file "${projectPath}mysite/wsgi.py" &
+
+sudo service nginx reload
