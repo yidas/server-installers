2b3f710907a6eaa6e422c4791ae9b9f8bb94bc16
Nick Tsai <myintaer@gmail.com>
2019-04-24 13:59:18 +0800

    Update ubuntu18.04.sh

--- redis/ubuntu18.04.sh
+++ redis/ubuntu18.04.sh
@@ -8,7 +8,7 @@
 
 # Password
 echo "Redis: Type the password for Redis, default is no authentication followed by [ENTER]:"
-read redisPassword
+read -s redisPassword
 
 # phpRedisAdmin
 installAdmin=false;
@@ -26,17 +26,12 @@ if [ $installAdmin = true ]; then
 
     if [ $adminUsername ]; then
         echo "phpRedisAdmin: Type the password for phpRedisAdmin, followed by [ENTER]:"
-        read adminPassword
+        read -s adminPassword
     fi
 fi
 
 # APT Source using IPv4
 sudo apt update
-
-# Timezone
-sudo timedatectl set-timezone Asia/Taipei
-sudo apt install ntpdate -y
-sudo ntpdate time.stdtime.gov.tw
 
 # Redis
 sudo apt install redis-server -y
